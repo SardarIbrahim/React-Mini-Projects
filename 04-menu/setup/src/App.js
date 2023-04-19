c3a8d9b99ea315dd478c92633ba2a5bcc9a562ab
SardarIbrahim <KhanzadaIbrahim05@gmail.com>
2023-04-20 00:26:44 +0500

    uploaded mini projects

---
+++ 04-menu/setup/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = [
+  'all',
+  ...new Set(
+    items.map((item, index) => {
+      return item.category;
+    })
+  ),
+];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item, index) => {
+      return item.category === category;
+    });
+
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
